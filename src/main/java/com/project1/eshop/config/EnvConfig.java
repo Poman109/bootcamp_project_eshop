6f2d669da44f134c09391057f57092b10c6a300a
Poman109 <mancy109@hotmail.com>
2023-07-27 12:21:38 +0800

    Added CORS Config 2

--- src/main/java/com/project1/eshop/config/EnvConfig.java
+++ src/main/java/com/project1/eshop/config/EnvConfig.java
@@ -1,5 +1,8 @@
 package com.project1.eshop.config;
 
+
+
+
 public class EnvConfig {
     public static final String devConfig = "http://localhost:5173";
     public static final String prodConfig = "http://fsse2305-project-mandy109.s3-website-ap-southeast-1.amazonaws.com";
